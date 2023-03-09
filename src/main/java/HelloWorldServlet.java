3db5d2e4d7e1b30ff6b2018e01db6968c66771af
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-09 11:37:07 -0600

    Refactor HelloWorldServlet - add hello.jsp for presentation - pass control flow from HelloWorldServlet to hello.jsp

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,20 +1,22 @@
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello"})
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        String name = "World";
-        if(req.getParameterMap().containsKey("name")) {
-            name = req.getParameter("name");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        String name = req.getParameter("name");
+        if( name == null) {
+            name = "World";
+        } else if ( "bgates".equalsIgnoreCase(name) ) {
+            res.sendRedirect("https://www.microsoft.com");
+            return;
         }
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-        out.println(String.format("<h1>Hello, %s!</h1>", name));
+        req.setAttribute("name", name);
+        req.getRequestDispatcher("/hello.jsp").forward(req, res);
     }
 }
