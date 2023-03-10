691cb44e21ab2ded7abf3ec574283c7af09e5fa4
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-10 10:52:21 -0600

    Refactor CountServlet - replaced hard-coded HTML with redirect to count.jsp

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -1,23 +1,23 @@
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "CountServlet", urlPatterns = {"/count"})
 public class CountServlet extends HttpServlet {
+
     private int counter = 0;
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
         counter = "true".equalsIgnoreCase(req.getParameter("reset"))
                 ? 0
                 : counter;
 
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-        out.println(String.format("<h1>Count: %d</h1>", ++counter));
+        req.setAttribute("counter", ++counter);
+        req.getRequestDispatcher("/count.jsp").forward(req, res);
     }
 }
