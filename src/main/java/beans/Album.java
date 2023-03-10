114c8e95f477fdcbc239f5e8b7cc757720cfc72e
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-10 11:20:16 -0600

    Refactor Album bean - replace java.util.Date with java.time.LocalDate - add additional tests

--- src/main/java/beans/Album.java
+++ src/main/java/beans/Album.java
@@ -4,20 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Album implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
     private long id;
     private String artist;
     private String name;
-    private Date release_date;
+    private LocalDate release_date;
     private BigDecimal Sales;
     private String genre;
 }
--- src/test/java/beans/BeanTest.java
+++ src/test/java/beans/BeanTest.java
@@ -3,6 +3,8 @@ package beans;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +13,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BeanTest {
 
     @Test
-    public void testGetAlbumId() {
+    public void testAlbum() {
         Album album = new Album();
         album.setId(1L);
+        album.setName("The Dark Side of the Moon");
+        album.setArtist("Pink Floyd");
+        album.setRelease_date(LocalDate.of(1973, 3, 1));
+        album.setSales(BigDecimal.valueOf(45000000));
+        album.setGenre("ROCK");
+
         assertEquals(1L, album.getId());
+        assertEquals("The Dark Side of the Moon", album.getName());
+        assertEquals("Pink Floyd", album.getArtist());
+        assertEquals(LocalDate.of(1973, 3, 1), album.getRelease_date());
+        assertEquals(BigDecimal.valueOf(45000000), album.getSales());
+        assertEquals("ROCK", album.getGenre());
+    }
+
+    @Test
+    public void testAuthor() {
+        Author author = new Author("Albert", "Einstein");
+        assertEquals("Albert Einstein", author.getFullName());
+    }
+
+    @Test
+    public void testQuote() {
+        Author author = new Author("Albert", "Einstein");
+        Quote quote = new Quote(author, "The difference between stupidity and genius is that genius has its limits.");
+        assertEquals(author, quote.getAuthor());
+        assertEquals("The difference between stupidity and genius is that genius has its limits.", quote.getContent());
     }
 
     @Test
@@ -28,9 +55,7 @@ class BeanTest {
                 )
         );
 
-        quotes.forEach(quote -> {
-            System.out.println(String.format("%s: \"%s\"", quote.getAuthor().getFullName(), quote.getContent()));
-        });
+        quotes.forEach(quote -> System.out.printf("%s: \"%s\"%n", quote.getAuthor().getFullName(), quote.getContent()));
 
     }
 
