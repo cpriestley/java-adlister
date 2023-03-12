9e8d05f34d447e3cf9874e3106b88ccdd1550912
Clayton Priestley <clayton.priestley@gmail.com>
2023-03-12 18:52:41 -0500

    Refactor HelloWorldServlet - copying changes from another branch - delete ParameterTestServlet.java

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,17 +1,22 @@
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello"})
 public class HelloWorldServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, world!</h1>");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        String name = req.getParameter("name");
+        if( name == null) {
+            name = "World";
+        } else if ( "bgates".equalsIgnoreCase(name) ) {
+            res.sendRedirect("https://www.microsoft.com");
+            return;
+        }
+        req.setAttribute("name", name);
+        req.getRequestDispatcher("/hello.jsp").forward(req, res);
     }
 }
